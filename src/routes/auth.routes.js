c7ea52df85e1ee2d1a13cffea1fc716e2ba61040
InaasHammoush <121546232+InaasHammoush@users.noreply.github.com>
2025-11-19 11:10:53 +0100

    fix the routing for password reset

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -14,6 +14,6 @@ router.get('/verify-email/:token', AuthController.verifyEmail);
 router.patch('/change-password', authenticateToken, AuthController.changePassword);
 
 router.post('/request-password-reset', isAuthenticated, AuthController.resetPasswordRequest);
-router.post('/reset-password/:token', isAuthenticated, AuthController.resetPassword);
+router.post('/password/reset/:token', isAuthenticated, AuthController.resetPassword);
 
 export default router;
