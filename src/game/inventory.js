6405bcc2a47ea4fac6ac2443ca470ba6ab555468
InaasHammoush <121546232+InaasHammoush@users.noreply.github.com>
2026-03-05 22:47:57 +0100

    turn off inventory starter items for testing

--- src/game/inventory.js
+++ src/game/inventory.js
@@ -4,12 +4,12 @@ import { CONSUMPTION_RULES } from './puzzles/helper/consumptionRules.js';
 import { REWARD_RULES } from './puzzles/helper/rewardRules.js';
 
 export const STARTER_INVENTORY = Object.freeze({
-   MOONWORT: 1,
-   GREEN_LIQUID: 1,
-   COAL_BLOCK: 1,
-   GOLD_NUGGET: 1,
-   MATCHES: 1,
-   FEATHER: 1,
+   //MOONWORT: 1,
+   //GREEN_LIQUID: 1,
+   //COAL_BLOCK: 1,
+   //GOLD_NUGGET: 1,
+   //MATCHES: 1,
+   //FEATHER: 1,
 });
 
 
